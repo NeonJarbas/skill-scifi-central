47b004219ea244dde17e1ecb0d4cc14dd703c94c
JarbasAi <jarbasai@mailfence.com>
2024-01-09 21:15:54 +0000

    drop "ui" folder

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill
 class SciFiCentralSkill(OVOSCommonPlaybackSkill):
     def __init__(self, *args, **kwargs):
         self.supported_media = [MediaType.MOVIE]
-        self.skill_icon = join(dirname(__file__), "ui", "scificentral_icon.jpg")
+        self.skill_icon = join(dirname(__file__), "res", "scificentral_icon.jpg")
         self.archive = JsonStorageXDG("SciFiCentral", subfolder="OCP")
         super().__init__(*args, **kwargs)
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
     package_dir={"skill_scifi_central": ""},
-    package_data={'skill_scifi_central': ['locale/*', 'ui/*']},
+    package_data={'skill_scifi_central': ['locale/*', 'res/*']},
     packages=['skill_scifi_central'],
     include_package_data=True,
     install_requires=["ovos_workshop~=0.0.5a1"],
